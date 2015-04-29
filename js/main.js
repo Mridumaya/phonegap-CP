a9418980d564ffd8513ace5ec52603bcb0354475
Mridula <mridumaya@gmail.com>
2015-04-29 07:46:33 +0530

    Changes to make app look native and single page

--- js/main.js
+++ js/main.js
@@ -14,9 +14,34 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
-        $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+        //this.store = new MemoryStore();
+        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
+		var self = this;
+		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+			self.renderHomeView();
+		});
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+    },
+	
+		showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
+	
+	renderHomeView: function() {
+		var html =
+				"<div class='header'><h1>Home</h1></div>" +
+				"<div class='search-view'>" +
+				"<input class='search-key'/>" +
+				"<ul class='employee-list'></ul>" +
+				"</div>"
+		$('body').html(html);
+		$('.search-key').on('keyup', $.proxy(this.findByName, this));
+	}
 
 };
 
