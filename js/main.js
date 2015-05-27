e886a835fb204b8111a0b2653d4bbd1e013dd068
Mridula <mridumaya@gmail.com>
2015-05-27 17:26:03 +0530

    Made it back to original

--- js/main.js
+++ js/main.js
@@ -2,50 +2,21 @@ var app = {
 
     findByName: function() {
         console.log('findByName');
-		var self = this;
         this.store.findByName($('.search-key').val(), function(employees) {
-            //var l = employees.length;
-            //var e;
-            //$('.employee-list').empty();
-            //for (var i=0; i<l; i++) {
-            //    e = employees[i];
-            //    $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
-            //}
-			$('.employee-list').html(self.employeeLiTpl(employees));
+            var l = employees.length;
+            var e;
+            $('.employee-list').empty();
+            for (var i=0; i<l; i++) {
+                e = employees[i];
+                $('.employee-list').append('<li><a href="#employees/' + e.id + '">' + e.firstName + ' ' + e.lastName + '</a></li>');
+            }
         });
     },
 
     initialize: function() {
-        //this.store = new MemoryStore();
-        //$('.search-key').on('keyup', $.proxy(this.findByName, this));
-		var self = this;
-		this.store = new MemoryStore(function() {
-			self.showAlert('Store Initialized', 'Info');
-			self.renderHomeView();
-		});
-		this.homeTpl = Handlebars.compile($("#home-tpl").html());
-		this.employeeLiTpl = Handlebars.compile($("#employee-li-tpl").html());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-    },
-	
-	showAlert: function (message, title) {
-		if (navigator.notification) {
-			navigator.notification.alert(message, null, title, 'OK');
-		} else {
-			alert(title ? (title + ": " + message) : message);
-		}
-	},
-	
-	renderHomeView: function() {
-		//var html =
-		//		"<div class='header'><h1>Home</h1></div>" +
-		//		"<div class='search-view'>" +
-		//		"<input class='search-key'/>" +
-		//		"<ul class='employee-list'></ul>" +
-		//		"</div>"
-		$('body').html(this.homeTpl());
-		$('.search-key').on('keyup', $.proxy(this.findByName, this));
-	}
+        this.store = new MemoryStore();
+        $('.search-key').on('keyup', $.proxy(this.findByName, this));
+    }
 
 };
 
