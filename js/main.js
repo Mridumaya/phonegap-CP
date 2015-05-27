2cd88e5bc9b528132d7354e21aecda9a791ce4c9
Mridula <mridumaya@gmail.com>
2015-05-27 17:42:13 +0530

    Added native notification

--- js/main.js
+++ js/main.js
@@ -12,9 +12,21 @@ var app = {
             }
         });
     },
+	
+	showAlert: function (message, title) {
+		if (navigator.notification) {
+			navigator.notification.alert(message, null, title, 'OK');
+		} else {
+			alert(title ? (title + ": " + message) : message);
+		}
+	},
 
     initialize: function() {
+		var self = this;
         this.store = new MemoryStore();
+		this.store = new MemoryStore(function() {
+			self.showAlert('Store Initialized', 'Info');
+		});
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
     }
 
